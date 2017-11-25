57e4cc958f2607baad65e304d931f40dd449f318
yuqiliao <yuqiliao@hotmail.com>
2017-11-25 14:21:58 -0500

    read-in experiment

--- Master.R
+++ Master.R
@@ -4,4 +4,17 @@
 #Sifan Liu
 
 #Capital Bikeshare
-# text git
+
+library('dplyr')
+install.packages('purrr')
+library('purrr')
+
+setwd("/Users/Yuqi/Google Drive/Skyladder/Data/Capital Bikeshare Trip History Data/Unzipped")
+
+## Read in unzipped individual data files
+allfiles <- list.files(path = ".",full.names = TRUE, all.files = FALSE)
+bikedata <- lapply(allfiles, read.csv, sep = ',', header = TRUE)
+names(bikedata) <- allfiles
+
+ex1 <- read.csv(file = "./2010-Q4-cabi-trip-history-data.csv", sep = ",", header = TRUE)
+ex2 <- read.csv(file = "./2011-Q1-cabi-trip-history-data.csv", sep = ",", header = TRUE)
